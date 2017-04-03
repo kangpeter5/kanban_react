da90e289ce33fe766cc9106ab2847e34e849c3ae
Peter Kang <peter.kang@cmlmediasoft.local>
2017-04-03 08:51:39 -0700

    adding Notes.jsx component

---
+++ app/components/Notes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import uuid from 'uuid';
+
+const notes = [
+	{
+		id: uuid.v4(),
+		task: 'Learn React'
+	},
+	{
+		id: uuid.v4(),
+		task: 'Do Laundry'
+	}
+];
+
+export default () => (
+  <ul>{notes.map(note =>
+    <li key={note.id}>{note.task}</li>
+  )}</ul>
+)
