fc84f54267bbad864a71dcb6025ab9520f3071f9
Peter Kang <peter.kang@cmlmediasoft.local>
2017-04-12 15:51:50 -0700

    fin

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -4,12 +4,12 @@ import {DragSource, DropTarget} from 'react-dnd';
 import ItemTypes from '../constants/itemTypes';
 
 const Note =({
-	connectDragSource, connectDropTarget, onMove, id, children, ...props
+	connectDragSource, connectDropTarget, isDragging, isOver, onMove, id, editing, children, ...props
 }) => {
-	return compose(connectDragSource, connectDropTarget)(
-		<div {...props}>
-			{children}
-		</div>
+	const dragSource = editing ? a => a : connectDragSource;
+
+	return compose(dragSource, connectDropTarget)(
+		<div style={{opacity: isDragging || isOver ? 0 : 1}} {...props}>{children}</div>
 	);
 }
 
@@ -34,10 +34,12 @@ const noteTarget = {
 };
 
 export default compose(
-	DragSource(ItemTypes.NOTE, noteSource, connect => ({
-		connectDragSource: connect.dragSource()
-	})),
-	DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
-		connectDropTarget: connect.dropTarget()
-	}))
+  DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  })),
+  DropTarget(ItemTypes.NOTE, noteTarget, (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  }))
 )(Note)
